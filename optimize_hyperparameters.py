f8b81b2e43c6033f694c4ac13cff67926d76b78e
Joshua-Schroijen <u0133040@kuleuven.be>
2020-12-18 13:33:51 +0100

    Implement hyperparameter optimization in random combination order

--- optimize_hyperparameters.py
+++ optimize_hyperparameters.py
@@ -18,13 +18,12 @@ if __name__ == '__main__':
 
     no_vertices = 140
     
-    tsps = [r0486848.TSP.get_random(no_vertices) for _ in range(5)]
+    tsps = [r0486848.TSP.get_random(no_vertices) for _ in range(3)]
 
     population_generation_schemes = [r0486848.PopulationGenerationScheme.RANDOM, r0486848.PopulationGenerationScheme.NEAREST_NEIGHBOUR_BASED]
     recombination_operators = [r0486848.RecombinationOperator.HGREX, r0486848.RecombinationOperator.PMX]
     elimination_schemes = [r0486848.EliminationScheme.LAMBDA_MU, r0486848.EliminationScheme.LAMBDAPLUSMU, r0486848.EliminationScheme.LAMBDAPLUSMU_WCROWDING]
-    # no_islands = [1, 2, 4, 5, 7, 10]
-    no_islands = [7, 10]
+    no_islands = [1, 2, 4, 5, 7, 10]
     island_swap_rates = [1, 3, 6]
     island_no_swapped_individuals = [1, 2, 4, 8]
     population_size_factors = [2, 3, 4, 5, 6]
@@ -40,14 +39,17 @@ if __name__ == '__main__':
     output_file.write('-' * 50 + '\n')
     print('-' * 50)
 
-    combinations = r0486848.Combinations(population_generation_schemes, recombination_operators, elimination_schemes, no_islands, island_swap_rates, island_no_swapped_individuals, population_size_factors, default_mutation_chances, mutation_chance_feedbacks, mutation_chance_self_adaptivities)
+    combinations = {i : c for i, c in enumerate(r0486848.Combinations(population_generation_schemes, recombination_operators, elimination_schemes, no_islands, island_swap_rates, island_no_swapped_individuals, population_size_factors, default_mutation_chances, mutation_chance_feedbacks, mutation_chance_self_adaptivities))}
     no_combinations = len(combinations)
     moving_average_time_per_combination = 0
 
-    for i, c in enumerate(combinations):
+    for i in range(no_combinations):
         start_time = timeit.default_timer()
 
-        current_combination = c[0:7] + (int((c[6] * no_vertices ) / (4 * c[3])), int((c[6] * no_vertices ) / c[3]), int((c[6] * no_vertices ) / c[3])) + c[7:] + (0.001, 3, False)
+        combination_choice_key = np.random.choice(list(combinations.keys()), 1)[0]
+        chosen_combination = combinations[combination_choice_key]
+        current_combination = chosen_combination[0:7] + (int((chosen_combination[6] * no_vertices ) / (4 * chosen_combination[3])), int((chosen_combination[6] * no_vertices ) / chosen_combination[3]), int((chosen_combination[6] * no_vertices ) / chosen_combination[3])) + chosen_combination[7:] + (0.001, 3, False)
+        del combinations[combination_choice_key]
 
         with multiprocessing.Pool() as p:
             res = p.apply_async(evaluate_combination, [tsps, current_combination])
@@ -88,7 +90,7 @@ if __name__ == '__main__':
 
     output_file.write('-' * 50 + '\n')
     print('-' * 50)
-    output_file.write(f'Best combination was\n\t{current_combination}\nBest combination average was\n\t{current_combination_average}\n')
-    print(f'Best combination was\n\t{current_combination}\nBest combination average was\n\t{current_combination_average}')
+    output_file.write(f'Best combination was\n\t{best_combination}\nBest combination average was\n\t{best_combination_average}\n')
+    print(f'Best combination was\n\t{best_combination}\nBest combination average was\n\t{best_combination_average}')
 
     output_file.close()
